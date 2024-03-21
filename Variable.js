1aa5138b97e73c11ad8fbdaf94236556e5b1774c
Padma Priyan <saravanapriyan30122003@gmail.com>
2024-03-21 19:13:37 +0530

    variables in javascript

---
+++ Variable.js
@@ -0,0 +1,3 @@
+var myName = "ssaravana";
+var yourName = prompt("what is your name");//prompt allows you to type 
+alert("my name is"+myName+"welcome Mr/Ms"+yourName);//alert displays the statement 
