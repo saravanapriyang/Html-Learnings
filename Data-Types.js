3244853dae36e76478105012abf8c062de77245f
Padma Priyan <saravanapriyan30122003@gmail.com>
2024-03-21 19:05:22 +0530

    data types in javascript

---
+++ Data-Types.js
@@ -0,0 +1,14 @@
+/*Goto console in the developer option in the web browser*/
+alert("Hello")
+//returns Hello
+typeof(23);
+//return number
+typeof("sp");
+//return string
+
+/* Data types in JS
+string 
+Boolean
+null
+number 
+*/
